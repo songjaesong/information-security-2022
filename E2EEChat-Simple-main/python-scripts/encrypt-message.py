10ab20841a98c8d88d7317cb801c09839997b56b
songjaesong <95694013+songjaesong@users.noreply.github.com>
2022-11-29 19:54:59 +0900

    implement E2EEChat
    implement E2EEChat

---
+++ E2EEChat-Simple-main/python-scripts/encrypt-message.py
@@ -0,0 +1,35 @@
+from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+import base64
+
+def decode_base64(b64):
+    return base64.b64decode(b64)
+
+def encode_base64(p):
+    return base64.b64encode(p).decode('ascii')
+
+def read_from_base64():
+    return [ decode_base64(input()), input() ]
+
+def pad_message(msg):
+    padded_msg = msg.encode('utf-8') # 메세지 패딩 구현
+    return pad(padded_msg, 16)
+
+def encrypt_message(key, iv, msg):
+    # AES 256 암호화 구현
+    # 위에서 먼저 padding을 해줘서 안해줘도됨
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = encode_base64(cipher.encrypt(msg))
+    
+    return ciphertext
+
+[secretkey, message] = read_from_base64()
+
+message = pad_message(message)
+randomiv = get_random_bytes(16) # 16바이트 (128비트 IV 랜덤 생성)
+
+randomiv_str = encode_base64(randomiv)
+cipher_str = encrypt_message(secretkey, randomiv, message)
+
+print(randomiv_str + '!' + cipher_str)
